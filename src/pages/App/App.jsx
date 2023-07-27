fff0f5c1097938af64c13480a37f405274389389
Stelusno <stelusno@gmail.com>
2023-07-27 15:03:53 -0400

    NavBar

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import NavBar from "./component/NavBar/NavBar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
