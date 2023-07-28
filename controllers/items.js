da4db579df7c4ae2a36c36bb2f06c8b66fe42f23
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-28 10:48:36 -0400

    Add routes and controllers

---
+++ controllers/items.js
@@ -0,0 +1,57 @@
+const Item = require('../models/item')
+
+module.exports = {
+	index,
+	show,
+	create,
+	edit,
+	deleteItem,
+}
+
+async function index(req, res) {
+	try {
+		const menuItems = await Item.find({});
+		res.status(200).json(menuItems);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+async function show(req, res) {
+	const id = req.params.itemId
+	try {
+		const menuItem = await Item.findById(id);
+		res.status(200).json(menuItem);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+async function create(req, res) {
+	try {
+		const item = await Item.create(req.body);
+		res.status(201).json(item)
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+async function edit(req, res) {
+	const id = req.params.itemId
+	try {
+		const menuItem = await Item.findOneAndUpdate(id, req.body);
+		res.status(202).json(menuItem);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+async function deleteItem(req, res) {
+	const id = req.params.itemId
+	try {
+		await Item.findOneAndDelete(id);
+		res.status(204);
+	} catch (err) {
+		console.log(err);
+	}
+}
