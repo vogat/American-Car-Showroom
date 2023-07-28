ae78736bd1b86b45a7f24956a1ab5e3203d2caf9
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-28 18:54:17 -0400

    Add server routes and controllers for Orders

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const menuItemController = require('../controllers/items')
+const menuItemController = require('../controllers/items');
+const orderController = require('../controllers/orders');
 
 //			'/api'
 
@@ -12,6 +13,18 @@ router.delete('/items/:itemId', menuItemController.deleteItem);
 router.put('/items/:itemId', menuItemController.edit);
 
 router.post('/items/', menuItemController.create);
+
+
+
+router.get('/orders/', orderController.index);
+router.get('/orders/:orderId', orderController.show);
+
+router.delete('/orders/:orderId', orderController.deleteItem);
+
+router.put('/orders/:orderId', orderController.edit);
+
+router.post('/orders/', orderController.create);
+
 
 
 module.exports = router;
