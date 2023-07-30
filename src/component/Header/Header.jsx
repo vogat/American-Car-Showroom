5064d9ec985e69c36a9efb03f8368b7c07391a83
vogat <james.voight@me.com>
2023-07-30 18:36:52 -0400

    user and home page made

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,5 +1,7 @@
+import './Header.css'
+
 export default function Header() {
     return(
-        <h1>The Melting Pot Restaurants</h1>
+        <div className="Header"><h1>The Melting Pot Restaurant</h1></div>
     )
 }
--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -1,18 +1,23 @@
 import Item from '../../component/Item/Item';
 import { Fragment } from 'react';
+import './Menu.css';
 
 export default function Menu({ items }) {
     const allItems = items.map(item => {
         return (
-            <Fragment key={item._id}>
-                <Item item={item} />
-                <hr />
-            </Fragment>
+                <Fragment key={item._id}>
+                    <Item item={item} />
+                </Fragment>
         );
     });
     return(
         <>
-			{allItems}
-		</>
+            <div className='MenuArea'>
+                <h2>Menu</h2>
+                <div className='Menu'>
+                    {allItems}
+                </div>
+            </div>
+        </>
     )
 }
