cbff18027f277076992b9718b10fd5d6b44e2ec1
vogat <james.voight@me.com>
2023-07-31 16:22:30 -0400

    ff

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api', apiRouter);
