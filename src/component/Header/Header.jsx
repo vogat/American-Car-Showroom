d2ecfc875906db3b26f4eefa9916a4e9f0254965
vogat <james.voight@me.com>
2023-08-08 14:45:19 -0400

    init

--- src/component/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import './Header.css'
-
-export default function Header() {
-    return(
-        <div className="Header"><h1>The Melting Pot Restaurant</h1></div>
-    )
-}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,114 +1,12 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import NavBar from "../../component/NavBar/NavBar";
-import AddItem from '../../component/AddItem/AddItem';
-import EditItem from '../../component/EditItem/EditItem';
-import Item from '../../component/Item/Item';
-import ItemIndex from '../../component/ItemIndex/ItemIndex';
-import OrderIndex from '../../pages/OrderIndex/OrderIndex';
-import OrderItem from '../../component/OrderItem/OrderItem';
-import LoginPage from '../../component/LoginPage/LoginPage'
-import Home from '../../pages/Home/Home';
-
 import { Routes, Route } from 'react-router-dom';
 import { Fragment } from 'react';
 
 export default function App() {
-	const [items, setItems] = useState([]);
-	const [orders, setOrders] = useState([]);
-
-	const [user, setUser] = useState(null);
-
-	async function getItems() {
-		try {
-			const response = await axios.get('https://meltinpot.onrender.com/api/items/');
-			setItems(response.data);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	async function getOrders() {
-		try {
-			const response = await axios.get('https://meltinpot.onrender.com/api/orders/');
-			setOrders(response.data);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	async function handleCreate(createdItem) {
-		try {
-			const response = await axios.post('https://meltinpot.onrender.com/api/items/', createdItem);
-			setItems([...items, response.data]);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	useEffect(() => {
-		getItems();
-		getOrders();
-	}, []);
-
-	async function handleDelete(deletedItem) {
-		try {
-			await axios.delete(`https://meltinpot.onrender.com/api/items/${deletedItem._id}/`);
-			const notDeletedItems = items.filter(item => item._id !== deletedItem._id)
-			setItems(notDeletedItems);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-
-	async function handleEdit(editedItem) {
-		try {
-			await axios.put(`https://meltinpot.onrender.com/api/items/${editedItem._id}/`, editedItem);
-			const newItems = items.map(i => {
-				return i._id !== editedItem._id ? i : editedItem;
-			});
-			setItems(newItems);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-	
-	if (user === 'admin123') {
-		return(
-			<main className="App">
-				<>
-					<NavBar />
-					<Routes>
-						{/* <Route path='/' element={<Menu items={items} />} /> */}
-						<Route path='/' element={
-							<>
-								<AddItem handleCreate={handleCreate} />
-								<ItemIndex items={items} handleEdit={handleEdit} handleDelete={handleDelete}/>
-							</>}>
-						</Route>
-					</Routes>
-				</>
-			</main>
-		);
-	} else if (!user) {
-		return(
-			<main className="App">
-				<LoginPage setUser={setUser} />
-			</main>
-		);
-	} else {
-		return(
-			<main className="App">
-				<>
-					<Routes>
-						<Route path='/' element={<Home items={items} />} />
-						<Route path='/orders' element={<OrderIndex orders={orders} setOrders={setOrders} />} />
-						<Route path='/orders/:orderId' element={<OrderItem />}/>
-					</Routes>
-				</>
-			</main>
-		);
-	}
+	return(
+		'Hello'
+	)
 }
 
